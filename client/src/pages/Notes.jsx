3c18996748b963dc533b934620cf26faf8f87c4b
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2025-11-20 23:37:06 +0530

    Update Notes.jsx

--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -37,7 +37,11 @@ export default function NotesPage() {
     if (!token) return alert("Please login first");
     // Open backend auth route in new tab
     // NOTE: Ensure this matches your backend URL (localhost:9000)
-    window.open(`https://scribly-backend-new.onrender.com/auth/google?token=${token}`, "_blank");
+    window.open(
+  `https://scribly-backend-new.onrender.com/auth/google?token=${encodeURIComponent(token)}`,
+  "_blank"
+);
+
   };
 
   const fetchNotes = async () => {
@@ -400,6 +404,7 @@ const handleSubmit = async (e) => {
     </div>
   );
 }
+
 
 
 
